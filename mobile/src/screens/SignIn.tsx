15db11269cdd19a9ee02a9749664b7d8ddacf74a
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-05 16:55:22 -0300

    Contexto de autenticação

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -2,13 +2,16 @@ import { Fontisto } from "@expo/vector-icons"
 import { Center, Icon, Text } from "native-base"
 import Logo from "../assets/logo.svg"
 import { Button } from "../components/Button"
+import { useAuth } from "../hooks/useAuth"
 
 export function SignIn() {
+  const { signIn } = useAuth()
   return (
     <Center flex={1} bg="gray.900" p={7}>
       <Logo width={212} height={40} />
 
       <Button
+        onPress={signIn}
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type="SECONDARY"
         title="ENTRAR COM GOOGLE"
