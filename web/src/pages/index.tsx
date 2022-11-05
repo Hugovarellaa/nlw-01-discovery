53907946da570cec447b160bf700c793179dddf4
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-05 14:07:23 -0300

    Layout finished

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,4 +1,9 @@
 import { GetServerSideProps } from 'next'
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import iconCheck from '../assets/icon-check.svg'
+import logoImg from '../assets/logo.svg'
+import avatarUser from '../assets/users-avatar-example.png'
 
 interface HomeProps {
   count: string
@@ -6,9 +11,63 @@ interface HomeProps {
 
 export default function Home({ count }: HomeProps) {
   return (
-    <div>
-      <h1>Primeiro log</h1>
-      <h1>{count}</h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
+      <main>
+        <Image src={logoImg} alt="" />
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={avatarUser} alt="" />
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592 </span>
+            pessoas já estão usando
+          </strong>
+        </div>
+
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
+            type="text"
+            required
+            placeholder="Qual nome do seu bolão?"
+          />
+          <button
+            className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
+            type="submit"
+          >
+            CRIAR MEU BOLÃO
+          </button>
+        </form>
+
+        <p className="mt-4 text-gray-300 text-sm leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas 🚀
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex justify-between text-gray-100">
+          <div className="flex items-end gap-6">
+            <Image src={iconCheck} alt="Image de check" />
+            <div className="flex flex-col">
+              <strong className="font-bold text-2xl">+2.034</strong>
+              <span>Bolões criados </span>
+            </div>
+          </div>
+
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-end gap-6">
+            <Image src={iconCheck} alt="Image de check" />
+            <div className="flex flex-col">
+              <strong className="font-bold text-2xl">+192.847</strong>
+              <span>Palpites enviados </span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image src={appPreviewImg} alt="" />
     </div>
   )
 }
