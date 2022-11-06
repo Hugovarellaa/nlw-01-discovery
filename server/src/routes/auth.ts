725cf1baa61e8a36b1142a4a42990f8562e7cc96
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-06 09:54:25 -0300

    Usando o access_token do google

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,3 +1,33 @@
+import axios from 'axios'
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 
-export async function authRoutes(fastify: FastifyInstance) {}
+export async function authRoutes(fastify: FastifyInstance) {
+  fastify.post('/users', async (request, reply) => {
+    const createUserBody = z.object({
+      access_token: z.string(),
+    })
+
+    const { access_token } = createUserBody.parse(request.body)
+
+    const userResponse = await axios.get(
+      'https://www.googleapis.com/oauth2/v2/userinfo',
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      },
+    )
+
+    const userInfoSchema = z.object({
+      id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      picture: z.string().url(),
+    })
+
+    const userInfo = userInfoSchema.parse(userResponse.data)
+
+    return { userInfo }
+  })
+}
