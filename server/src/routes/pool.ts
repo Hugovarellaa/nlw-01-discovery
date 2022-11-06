a443de0296efe412f22b8354be7295d33df3c1ab
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-11-06 11:12:56 -0300

    Criando bolao com usuario autenticado

--- server/src/routes/pool.ts
+++ server/src/routes/pool.ts
@@ -20,19 +20,28 @@ export async function poolRoutes(fastify: FastifyInstance) {
     const generate = new ShortUniqueId({ length: 6 })
     const code = String(generate()).toUpperCase()
 
-    const ownerId = null
-
     try {
-      await request.jwtVerify()
-
       // tenho um usuário autenticado
+      await request.jwtVerify()
+      await prisma.pool.create({
+        data: {
+          title,
+          code,
+          ownerId: request.user.sub,
+
+          participants: {
+            create: {
+              userId: request.user.sub,
+            },
+          },
+        },
+      })
     } catch {
       // nao tenho um usuário autenticado ai eu crio
       await prisma.pool.create({
         data: {
           title,
           code,
-          ownerId: request.user.sub,
         },
       })
     }
