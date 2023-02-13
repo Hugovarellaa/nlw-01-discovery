3437fb1abaa724da13902296e06f640aee022cbe
Hugo Alves Varella <hugovarellaa@gmail.com>
2023-02-13 15:39:01 -0300

    Using nunjucks in html

---
+++ README.md
@@ -0,0 +1,18 @@
+# Plugin sugeridos
+
+- Nunjucks Template
+
+* Configurações
+
+```
+  "files.associations": {
+  "\*.html": "njk"
+  },
+```
+
+```
+"emmet.includeLanguages": {
+"njk": "html"
+},
+
+```
--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 
 const app = express();
+
 app.use("/public", express.static("public"));
 
+nunjucks.configure("src/views", {
+  express: app,
+  noCache: true,
+});
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.render("index.html");
 });
 
 app.get("/create", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html");
+  res.render("create-point.html");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search.htm");
 });
 
 app.listen(3333, console.log("Server listening on port 3333"));
